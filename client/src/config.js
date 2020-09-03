389cf4b564c022e7545a6ff3ac1fe589010f1681
kevinngth <kevin.ng.th@gmail.com>
2020-09-03 21:08:56 +0800

    fix bug

--- client/src/config.js
+++ client/src/config.js
@@ -4,7 +4,7 @@ let API;
 if (process.env.NODE_ENV === "development") {
     API = "http://localhost:3000/api";
 } else {
-    API = "https://seif-3.herokuapp.com/api";
+    API = "https://seif-3-server.herokuapp.com/api";
 }
 
 export { API, BATCH };
