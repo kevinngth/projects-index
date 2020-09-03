fe1f899bdb4dbc5d7e36f4216939fe4e22c427dd
kevinngth <kevin.ng.th@gmail.com>
2020-09-03 21:06:45 +0800

    try crazy thing

---
+++ server/build/precache-manifest.0b86735f2fa4501a1ef7bd60f5cfd5d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce882c3a5e351be70c855874ce397a8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82fb7b17958e04df8694",
+    "url": "/static/css/main.2404b2f3.chunk.css"
+  },
+  {
+    "revision": "3656211573588181b6b6",
+    "url": "/static/js/2.9819dd4d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9819dd4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82fb7b17958e04df8694",
+    "url": "/static/js/main.c656a7ea.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 const { PORT } = process.env;
 const app = express();
@@ -10,6 +11,11 @@ const router = require("./routes/student");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
